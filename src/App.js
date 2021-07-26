c9d2e9b6e63240cc9374640184a9533f0daf21f5
eloyms262 <eloyMs26@gmail.com>
2021-07-26 10:58:31 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
+  );
+}
+
+function Apper(){
+  return(
+    <div>
+      <h2> Hello This is another div</h2>
+    </div>
+  );
+}
+
+export default App;
